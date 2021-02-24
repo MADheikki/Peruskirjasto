420af34fd930c51fd02f3e99bc2a00bd9f4e628a
Heikki Malkki <heikki@gdlab.fi>
2021-02-24 14:50:26 +0200

    tee_gsm.sh

---
+++ tee_gsm.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+LP_XMLConverter23="/Applications/GRAPHISOFT/ARCHICAD 23/ARCHICAD 23.app/Contents/MacOS/LP_XMLConverter.app/Contents/MacOS/LP_XMLConverter"
+
+"$LP_XMLConverter23" x2l Peruskirjasto_xml Peruskirjasto
