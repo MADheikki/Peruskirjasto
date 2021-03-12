9b8b3b78a2f5ed3c86f7a73377460a2dad1b353d
Heikki Malkki <heikki@gdlab.fi>
2021-03-12 08:51:40 +0200

    Kuvat ja koodi erikseen (-img)

--- tee_gsm.sh
+++ tee_gsm.sh
@@ -2,4 +2,4 @@
 
 LP_XMLConverter23="/Applications/GRAPHISOFT/ARCHICAD 23/ARCHICAD 23.app/Contents/MacOS/LP_XMLConverter.app/Contents/MacOS/LP_XMLConverter"
 
-"$LP_XMLConverter23" x2l Peruskirjasto_xml Peruskirjasto
+"$LP_XMLConverter23" x2l -img Peruskirjasto_img Peruskirjasto_xml Peruskirjasto
--- tee_xml.sh
+++ tee_xml.sh
@@ -2,4 +2,4 @@
 
 LP_XMLConverter23="/Applications/GRAPHISOFT/ARCHICAD 23/ARCHICAD 23.app/Contents/MacOS/LP_XMLConverter.app/Contents/MacOS/LP_XMLConverter"
 
-"$LP_XMLConverter23" l2x Peruskirjasto Peruskirjasto_xml
+"$LP_XMLConverter23" l2x -img Peruskirjasto_img Peruskirjasto Peruskirjasto_xml
